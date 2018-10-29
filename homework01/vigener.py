ccf02927ec24af2b8e440d4fe8fce3c8c1c0bcdf
trangology <trangology@gmail.com>
2018-10-29 21:36:27 +0300

    Update vigener.py

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -1,13 +1,4 @@
 def encrypt_vigenere(plaintext, keyword):
-    """
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
-    # PUT YOUR CODE HERE
     ciphertext = ''
     if keyword == 'A' or keyword == 'a':
         ciphertext = plaintext
@@ -48,15 +39,6 @@ def encrypt_vigenere(plaintext, keyword):
     return ciphertext
 
 def decrypt_vigenere(ciphertext, keyword):
-    """
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    # PUT YOUR CODE HERE
     plaintext = ''
     if (keyword == 'A') or (keyword == 'a'):
         plaintext = ciphertext
