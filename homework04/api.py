0154d210005f0c783ed8716fe2348a3dd69b994d
trangology <trangology@gmail.com>
2018-12-17 23:08:55 +0300

    updated

--- homework04/api.py
+++ homework04/api.py
@@ -42,8 +42,7 @@ def get_friends(user_id, fields):
         'fields': fields
     }
 
-    query = "{domain}/friends.get?access_token={access_token}\
-            &user_id={user_id}&fields={fields}&v=5.92".format(**query_params)
+    query = "{domain}/friends.get?{access_token}\&user_id={user_id}&fields={fields}&v=5.92".format(**query_params)
     response = get(query).json()
 
     if 'response' in response:
@@ -73,8 +72,7 @@ def messages_get_history(user_id, offset=0, count=20):
         'count': count,
     }
 
-    url = "{domain}/messages.getHistory?access_token={access_token}\
-            &user_id={user_id}&offset={offset}&count={count}&v=5.92"
+    url = "{domain}/messages.getHistory?{access_token}\&user_id={user_id}&offset={offset}&count={count}&v=5.92"
     query = url.format(**query_params)
 
     messages = []
