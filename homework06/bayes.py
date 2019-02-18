e8879c5068ce7be59012988e5429d18bd7aefc87
trangology <trangology@gmail.com>
2019-02-19 00:23:40 +0300

    update

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,11 +1,36 @@
+word_list = []
+frequency_ham = []
+frequency_spam = []
+
+
+# in this case, we use Naive Bayes with Laplace smoothing
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
+    def __init__(self, alpha=1):
+        # alpha is a parameter estimation with add 1 smoothing
         self.alpha = alpha
 
     def fit(self, X, y):
         """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        index = -1
+        for i in range(len(X)):
+            flag_list = X[i].split()
+            for word in flag_list:
+                if word not in word_list:
+                    index += 1
+                    word_list.append(word)
+                    frequency_ham.append(0)
+                    frequency_spam.append(0)
+                    if y[i] == 'ham':
+                        frequency_ham[index] = 1
+                    if y[i] == 'spam':
+                        frequency_spam[index] = 1
+                else:
+                    if y[i] == 'ham':
+                        frequency_ham[index] += 1
+                    if y[i] == 'spam':
+                        frequency_spam[index] += 1
+        print(word_list)
 
     def predict(self, X):
         """ Perform classification on an array of test vectors X. """
@@ -14,4 +39,3 @@ class NaiveBayesClassifier:
     def score(self, X_test, y_test):
         """ Returns the mean accuracy on the given test data and labels. """
         pass
-
---
+++ homework06/compare_argoth_NB.py
@@ -0,0 +1,42 @@
+import csv
+import string
+from bayes import NaiveBayesClassifier
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+model_test = Pipeline([
+    ('vectorizer', TfidfVectorizer()),
+    ('classifier', MultinomialNB(alpha=0.05)),
+])
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
+
+
+def read_and_process_data():
+    with open("./data/SMSSpamCollection") as f:
+        data = list(csv.reader(f, delimiter="\t"))
+    X, y = [], []
+    for target, msg in data:
+        X.append(msg)
+        y.append(target)
+    X = [clean(x).lower() for x in X]
+    return X[:3900], y[:3900], X[3900:], y[3900:]
+
+
+if __name__ == "__main__":
+    X_train, y_train, X_test, y_test = read_and_process_data()
+
+    model = NaiveBayesClassifier(alpha=1)
+    model.fit(X_train, y_train)
+    model_test.fit(X_train, y_train)
+
+    result_1 = model.score(X_test, y_test)
+    result_2 = model_test.score(X_test, y_test)
+
+    print("Ok") if result_1 == result_2 else print("Failed")
