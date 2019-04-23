23deda56c954dde3611fd755f6e9a6e508307851
trangology <trangology@gmail.com>
2019-04-23 19:24:11 +0300

    add files

---
+++ elevennote/src/config/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'django.contrib.postgres',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+    }
+}
+
+ALLOWED_HOSTS = ['localhost:8000']
