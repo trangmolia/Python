936caa45e18ede983afa4566adc8cddb3a4c42e3
trangology <trangology@gmail.com>
2019-06-21 17:25:22 +0300

    upload static

---
+++ homework08/src/static/elevennote/main.js
@@ -0,0 +1,28 @@
+(function($) { $(function() {
+    // allow whole list item to note to be clickable
+    $("#notes li").click(function(){
+        location.assign($(this).attr("data-url"));
+    });
+    
+    // add Title placeholder
+    $("#id_title").attr("placeholder", "Title your note");
+
+    // add tag attributes
+    $("#id_tags").attr({
+        "placeholder": "Add your tags...",
+        "data-role": "tagsinput"
+    });
+    
+    // set sidebar height
+    $('#sidebar').css("height", window.innerHeight - 55 );
+
+    $(document).ready(function(){
+       $("#delete-note").click(function(e){
+            e.preventDefault();
+            if (window.confirm("Are you sure?")) {
+               $("#delete-note-form").submit();
+           }
+       });
+    });
+
+}); })(jQuery);
